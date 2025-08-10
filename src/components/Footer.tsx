0d47d0a13e66a605b89dd7ff25827795209cc25e
Kevin Cauto <kevinmcauto@gmail.com>
2025-08-10 19:06:26 -0400

    fix: Increase icon size in Footer component for better visibility

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,8 +14,8 @@ export default function Footer() {
               aria-label="Instagram"
             >
               <svg
-                width="36"
-                height="36"
+                width="40"
+                height="40"
                 viewBox="0 0 24 24"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
