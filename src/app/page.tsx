a535540dd47ad2bb0b9b53bf87586e835c34fc8c
Kevin Cauto <kevinmcauto@gmail.com>
2026-02-20 19:17:21 -0500

    chore: Remove Under Construction banner from homepage for a cleaner user experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -287,26 +287,6 @@ export default async function Home() {
       
       {/* Available Flash section */}
       <main className="container mx-auto p-4">
-        {/* Under Construction Banner */}
-        <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4 mb-6 rounded">
-          <div className="flex items-center justify-center">
-            <div className="flex-shrink-0">
-              <svg className="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
-              </svg>
-            </div>
-            <div className="ml-3">
-              <p className="text-sm text-yellow-700 text-center">
-                <strong>Under Construction:</strong> This section may not be up to date or include misinformation. Please view the Instagram flash stories for the most accurate information <a 
-                href="https://www.instagram.com/sweetpotatotat/" 
-                target="_blank" 
-                rel="noopener noreferrer" 
-                className="underline font-semibold">HERE</a> for now.
-              </p>
-            </div>
-          </div>
-        </div>
-        
         <h1 className="text-4xl font-light text-center my-8 text-[#414141]">Available Flash Designs</h1>
         <FlashGrid 
           images={visibleBlobs}
